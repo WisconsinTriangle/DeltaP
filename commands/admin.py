869c952b11af29ca5f1345ca5833e40ad9636782
Warner Vance <warnervance@icloud.com>
2025-03-27 12:19:13 -0700

    Added a command to shutdown the bot
    Signed-off-by: Warner Vance <warnervance@icloud.com>

--- commands/admin.py
+++ commands/admin.py
@@ -1,8 +1,11 @@
+import time
 from datetime import datetime
 
 import discord
 import pytz
 from discord.ext import commands
+
+from role.role_checking import check_info_systems_role
 
 
 def setup(bot: commands.Bot):
@@ -36,3 +39,31 @@ def setup(bot: commands.Bot):
 
         # Edit the original response with the embed
         await interaction.edit_original_response(content=None, embed=embed)
+
+    @bot.tree.command(name="shutdown", description="Shut the bot down remotely")
+    async def shutdown(interaction: discord.Interaction):
+        """
+        Sets up the bot with a remote shutdown command. Shuts down the bot securely
+        when the appropriate user permission is detected. The command ensures only
+        authorized roles can invoke shutdown.
+
+        Parameters
+        ----------
+        interaction : discord.Interaction
+            The interaction context for the shutdown command, typically initiated by
+            a user command.
+
+        Raises
+        ------
+        None
+
+        Returns
+        -------
+        None
+        """
+        if await check_info_systems_role(interaction) is False:
+            await interaction.response.send_message("You don't have permission to do that.")
+            return
+        await interaction.response.send_message("Shutting down...")
+        time.sleep(1)
+        await bot.close()
